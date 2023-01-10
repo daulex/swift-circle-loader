9d9894aa673f95671bef6a31875c4f5dc9b3cffc
Kirill G <kirillgalenko@gmail.com>
2023-01-10 13:27:22 +0200

    Initial Commit

---
+++ Spinning Circle/Spinning_CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spinning_CircleApp.swift
+//  Spinning Circle
+//
+//  Created by Kirills Galenko on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Spinning_CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
