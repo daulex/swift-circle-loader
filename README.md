dd76a982c62f94cb2f26d9a36e3f72901753934b
Kirill G <kirillgalenko@gmail.com>
2023-01-10 14:02:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Swift Spinning loader circle
+
+I saw this simple, but cool loader animation and decided to make it in SwiftUI as I'm learning it now.
+
+Really happy with how it looks, here's a gif with the result: 
+
+https://i.imgur.com/HHz0avx.gif
--- Spinning Circle/ContentView.swift
+++ Spinning Circle/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRotating = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            ZStack {
+                Circle()
+                    .strokeBorder(.black, lineWidth: 1)
+                Circle()
+                    .foregroundColor(.teal)
+                    .frame(width: 15, height: 15)
+                    .offset(x:0, y:-33)
+                
+            }
+            
+            .frame(width: 70, height: 70)
+            .padding()
+            .rotationEffect(.degrees(isRotating))
+            .onAppear {
+                withAnimation(.linear(duration: 1)
+                    .speed(1)
+                    .repeatForever(autoreverses: false)) {
+                        isRotating = 360.0
+                    }
+            }
         }
-        .padding()
+        .rotation3DEffect(
+            .degrees(60),
+            axis: (x: 1.0, y: 0.0, z: 0.0)
+        )
     }
 }
 
